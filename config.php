0aa9d06b9f540ae3962c57a4dba5510f78762bc6
Jason Anderson <jason@jasonjanderson.com>
2010-09-19 20:59:51 -0500

    Added maintenance mode functionality to bootstrap.inc

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ define('VERSION', 'beta');
 define('CONSUMER_KEY', 'iY5GwavKZc4yx3SA2Oz2IA');
 define('CONSUMER_SECRET', 'y617QtnBQaviFF3iBjYGPJIbH22dS79ki9upGT8OE');
 define('AVATAR_PER_TWEET', 15);
+define('IN_MAINTENANCE_MODE', true);
 
 if (STATE == 'production') {
   define('OAUTH_CALLBACK', 'http://slothnod.com/callback');
