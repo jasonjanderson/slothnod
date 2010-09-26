945edf592e64800fbeb7fed64f5ec44e29dd1461
Jason Anderson <jason@jasonjanderson.com>
2010-09-26 02:55:36 -0500

    Added ability to open a new window on clicking of a menu item

--- include/menu.php
+++ include/menu.php
@@ -1,12 +1,18 @@
 <?php
 
-function get_menu_item($title, $link, $active) {
+function get_menu_item($title, $link, $active, $new_window=false) {
   global $route;
+  $element = '<li';
 
   if ($route == $active) {
-    return "<li class=\"active\"><a href=\"" . $link . "\">" . $title . "</a></li>"; 
+    $element .=' class="active"';
   }
-  return "<li><a href=\"" . $link . "\">" . $title . "</a></li>";
+  $element .= '><a href="' . $link . '"';
+  if ($new_window == true) {
+    $element .= ' target="_blank"';
+  }
+  $element .= '>' . $title . '</a></li>';
+  return $element;
 }
 
 ?>
