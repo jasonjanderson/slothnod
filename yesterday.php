859825e10070d07e81cf7589e81d618fb1c954d1
Jason Anderson <jason@jasonjanderson.com>
2010-11-04 14:44:54 -0500

    Yesterday's leaderboard now shows tweets in the order they made the leaderboard AND their posting date.

--- yesterday.php
+++ yesterday.php
@@ -16,7 +16,7 @@ $sql = "SELECT SQL_CALC_FOUND_ROWS yesterday_board.status_id,
                 yesterday_board.anonymous,
 		yesterday_board.user_id,
 		yesterday_board.username,
-		yesterday_board.img_url FROM yesterday_board ORDER BY yesterday_board.leaderboard_date DESC";
+		yesterday_board.img_url FROM yesterday_board ORDER BY yesterday_board.leaderboard_date DESC, yesterday_board.post_date ASC";
 
 $pagination = new Pagination($connection=$db->get_connection(), $sql=$sql, $rows_per_page=50, $num_adjacent=3, $page_url_name='p', $url_base='yesterday');
 $result = $pagination->get_result();
